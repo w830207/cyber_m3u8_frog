113a1982efd5d198687c4b31c0d10715099e5b03
w830207 <44136372+w830207@users.noreply.github.com>
2022-11-22 13:47:30 +0800

    微調

--- routes/app/getUrls/index.dart
+++ routes/app/getUrls/index.dart
@@ -2,5 +2,5 @@ import 'package:cyber_m3u8_frog/maps.dart';
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
-  return Response(body: tvTypeList.toString());
+  return Response.json(body: {'tvTypeList': tvTypeList});
 }
